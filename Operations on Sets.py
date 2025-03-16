c2777edf76758e465995a7c5daa3dcff74d8c516
zaydbannya <zayd.bannya@gmail.com>
2025-03-16 13:24:46 +0000

    Operations on Sets

---
+++ Operations on Sets.py
@@ -0,0 +1,15 @@
+my_set={1 ,2 , 3}
+print(my_set)
+
+my_set={1.0,"Hello",(123)}
+print(my_set)
+
+my_set=set([1 ,2 ,3 ,4 ,3 ,2])
+print(my_set)
+
+num_set=set([0 ,1 ,3 ,4 ,5])
+print("original set:")
+print(num_set)
+num_set.pop()
+print("after removing yhe first element from the said set:")
+print(num_set,"\n")
